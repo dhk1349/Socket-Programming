a68bf69832be2035c3fd6eae359df2765adb0155
root <root@localhost.localdomain>
2019-07-20 13:19:15 -0400

    code update

--- ARP_spoofing1/main.cpp
+++ ARP_spoofing1/main.cpp
@@ -1,8 +1,21 @@
 #include<iostream>
 #include<pcap.h>
+using namespace std;
+int usage(){
+    cerr<<"Syntax: Send_arp <interface><sender IP><target IP>\n";
+    cerr<<"Sample: Send_arp wlan0 192.168.10.2 102.160.10.1\n";
+    return -1;
+}
+//Suppose victim's ip is given.
+int main(int argc, char* argv[]){
+    if(argc!=4){
+        usage();
+        return -1;
+    }
 
 
-int main(){
+    //code for catching mac addr
 
-
+    //Sending arp packet
+    return 0;
 }
