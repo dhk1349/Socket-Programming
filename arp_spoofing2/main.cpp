bf6773d153dc76f96c8474384520b059a71a78bb
root <dhk1349@naver.com>
2019-08-05 09:02:54 -0400

    update

---
+++ arp_spoofing2/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+conditions
+-> Use sender and target ONLY.
+->
+*/
+
+int usage(){}
+
+ int main(int argc, char* argv[])
+{
+    if(argc!=5)usage();
+    //detect my mac
+    //detect sender mac
+    //infect sender mac table.
+
+    //
+
+    return 0;
+}
